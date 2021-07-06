d67c6cf67d5fe61ad9498b9ebd079863ed2a0a91
duhanmin <627820752ab>
2021-07-06 16:41:12 +0800

    up Runtime

--- src/main/java/com/on/yarn/process/RuntimeUtil.java
+++ src/main/java/com/on/yarn/process/RuntimeUtil.java
@@ -1,7 +1,5 @@
 package com.on.yarn.process;
 
-import org.apache.commons.lang.StringUtils;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -43,12 +41,43 @@ public class RuntimeUtil {
         // 单条命令的情况
         if (1 == cmds.length) {
             final String cmd = cmds[0];
-            if (StringUtils.isBlank(cmd)) {
+            if (isBlank(cmd)) {
                 throw new NullPointerException("Command is blank !");
             }
             cmds = cmdSplit(cmd);
         }
         return cmds;
+    }
+
+    /**
+     * <p>Checks if a CharSequence is empty (""), null or whitespace only.</p>
+     *
+     * <p>Whitespace is defined by {@link Character#isWhitespace(char)}.</p>
+     *
+     * <pre>
+     * StringUtils.isBlank(null)      = true
+     * StringUtils.isBlank("")        = true
+     * StringUtils.isBlank(" ")       = true
+     * StringUtils.isBlank("bob")     = false
+     * StringUtils.isBlank("  bob  ") = false
+     * </pre>
+     *
+     * @param cs  the CharSequence to check, may be null
+     * @return {@code true} if the CharSequence is null, empty or whitespace only
+     * @since 2.0
+     * @since 3.0 Changed signature from isBlank(String) to isBlank(CharSequence)
+     */
+    public static boolean isBlank(final CharSequence cs) {
+        final int strLen = (cs == null ? 0 : cs.length());
+        if (strLen == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
