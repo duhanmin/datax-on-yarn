91972a00891ca6adea712664d07d2ac5d6d81747
duhanmin <hanmin.du@longbridge.sg>
2023-01-04 14:41:10 +0800

    sudo

--- src/main/java/com/on/yarn/datax/Executor.java
+++ src/main/java/com/on/yarn/datax/Executor.java
@@ -9,7 +9,7 @@ public interface Executor {
 
     default void init(String dataxHome, String path) {
         if (!FileUtil.exist(dataxHome)) {
-            String cmd = "mkdir -p /mnt/dss && cd /mnt/dss && hdfs dfs -get -f /tmp/linkis/hadoop/datax/datax.tar.gz . && tar -zxf datax.tar.gz";
+            String cmd = "sudo mkdir -p /mnt/dss && sudo chmod 777 /mnt/dss && cd /mnt/dss && hdfs dfs -get -f /tmp/linkis/hadoop/datax/datax.tar.gz . && tar -zxf datax.tar.gz";
             String cmdPath = path + "init_cmd.sh";
             FileUtil.writeUtf8String(cmd, cmdPath);
             Constants.exec("sh " + cmdPath);
